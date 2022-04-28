05ef025f0d9ca4a1a852e3c47fbdea944cae64b9
Mohammed Jewel <jewelnabin@gmail.com>
2022-04-29 00:31:17 +0600

    node server site is ready

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+// midleware
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send("Hello World");
+});
+
+const users = [
+    {id: 1, name: 'Shabana', email: 'sabana@gmail.com', phone: '09286743567'},
+    {id: 2, name: 'Shabnur', email: 'Shabnur@gmail.com', phone: '09286743567'},
+    {id: 3, name: 'Shahanaj', email: 'Shahanaj@gmail.com', phone: '09286743567'},
+    {id: 4, name: 'socurita', email: 'socuritagmail.com', phone: '09286743567'},
+    {id: 5, name: 'sohana', email: 'sohana@gmail.com', phone: '09286743567'},
+    {id: 6, name: 'sonia', email: 'sonia@gmail.com', phone: '09286743567'},
+    {id: 7, name: 'sonom', email: 'sonom@gmail.com', phone: '09286743567'},
+]
+
+app.get('/users', (req, res) => {
+    if (req.query.name) {
+        const search = req.query.name.toLowerCase();
+        const matched = users.filter(user => user.name.toLowerCase().includes(search));
+        res.send(matched);
+    }
+    else {
+        res.send(users);
+    }
+});
+
+app.get('/user/:id', (req, res) => {
+    console.log(req.params);
+    const id = parseInt(req.params.id);
+    const user = users.find(u => u.id === id);
+    res.send(user);
+});
+
+app.post('/user', (req, res) => {
+    console.log('request', req.body);
+    const user = req.body;
+    user.id = users.length + 1;
+    users.push(user);
+    res.send(user);
+});
+
+app.listen(port, () => {
+    console.log("Listening to Hello World", port);
+});
